6014c6dc936fbd1f03bcb66c96e7ece34558b606
afahd <afahd@plumgrid.com>
2016-09-22 04:33:42 -0400

    Update first.sh

--- first.sh
+++ first.sh
@@ -1,4 +1,7 @@
 stage 'build'
+git clone ssh://afahd@gerrit.plumgrid.com:29418/builder.git ~/work/builder_test
+cd ~/work/builder_test
+ls
 echo "Hello this is test file"
 stage 'pipeline'
 echo "testing"
