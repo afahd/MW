5fc48f1642885baf186723034dde02fc00b86c09
afahd <afahd@plumgrid.com>
2016-10-07 06:34:25 -0700

    Create jenkinshelpers.groovy

---
+++ groovy/jenkinshelpers.groovy
@@ -0,0 +1,15 @@
+import groovy.json.JsonSlurper
+
+static void clone_dependenices()
+{
+    File f = new File('dependencies.json')
+    def slurper = new JsonSlurper()
+    def jsonText = f.getText()
+    def json = slurper.parseText( jsonText )
+    json.each{
+        clone_dependenices(it.project,it.location,it.branch)
+        echo "Cloning dependencies for $project "
+        git branch: branch, url: location
+    }
+
+}
