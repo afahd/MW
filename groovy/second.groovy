a993967d465c88ba57d0c86c2861bd1dee10b2fa
afahd <afahd@plumgrid.com>
2016-10-10 13:18:01 -0700

    Update second.groovy

--- groovy/second.groovy
+++ groovy/second.groovy
@@ -1,35 +1,29 @@
-//@Grab(group='org.yaml', module='snakeyaml', version='1.17')
-//import org.yaml.snakeyaml.*
 @Grab(group='org.yaml', module='snakeyaml', version='1.17')
 import org.yaml.snakeyaml.*
-//echo "hello"
-//public class Dependency {
-    //public LinkedHashMap h1;
-    //Dependency(LinkedHashMap h1) {
-    //    this.h1 = h1
-  //  }
-//}
-//public static clone()
-//{
-  //  Yaml yaml = new Yaml();
-    //String project;
-    //String location;
-    //String branch;
-    //String build;
-    //InputStream input = new FileInputStream(new File("dependencies.yaml"));
-    //Dependency data = yaml.load(input);
-    //println(data.h1.get('tools'))
-    //Set s1 = data.h1.keySet();
-    //for (int num=0;num<s1.size();num++)
-    //{
-      //  project = s1.toArray()[num];
-       // location = data.h1.get(project)['location'];
-       // branch = data.h1.get(project)['branch'];
-       // build = data.h1.get(project)['build'];
-
-        //echo "Cloning dependencies for $project "
-        //git branch: branch, url: location
-   // }
-//}
-
-//return this;
+public class Dependency {
+    public LinkedHashMap h1;
+    Dependency(LinkedHashMap h1) {
+        this.h1 = h1
+    }
+}
+public static clone()
+{
+   Yaml yaml = new Yaml();
+   String project;
+   String location;
+   String branch;
+   String build;
+   InputStream input = new FileInputStream(new File("dependencies.yaml"));
+   Dependency data = yaml.load(input);
+   Set s1 = data.h1.keySet();
+   for (int num=0;num<s1.size();num++)
+   {
+       project = s1.toArray()[num];
+       location = data.h1.get(project)['location'];
+       branch = data.h1.get(project)['branch'];
+       build = data.h1.get(project)['build'];
+       echo "Cloning dependencies for $project "
+       git branch: branch, url: location
+    }
+}
+return this;
