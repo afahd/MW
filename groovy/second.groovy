6c9a77fd383acec59885eab21b19990eb5a9135f
afahd <afahd@plumgrid.com>
2016-10-10 05:12:27 -0700

    Update second.groovy

--- groovy/second.groovy
+++ groovy/second.groovy
@@ -1,5 +1,5 @@
 @Grab(group='org.yaml', module='snakeyaml', version='1.5')
-
+import org.yaml.snakeyaml.*
 def yaml = new Yaml()
 def document = "\n- Hesperiidae\n- Papilionidae\n- Apatelodidae\n- Epiplemidae"
 list = (List<String>) yaml.load(document)
